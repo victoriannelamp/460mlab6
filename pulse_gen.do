a3e22f516757cd1ad4d2eb73534a19ca302adef3
victoriannelamp <alampman@utexas.edu>
2018-04-19 19:47:04 -0500

    more pulse stuff to incorporate hybrid

---
+++ pulse_gen.do
@@ -0,0 +1,5 @@
+force clk 0 0, 1 5 -r 10
+force rst 0 0, 1 10, 0 50 
+force start 0 0, 1 70, 0 2000
+force mode 0 0, 1 1000 
+run 10000
--- pulse_gen_clk_div.v
+++ pulse_gen_clk_div.v
@@ -5,20 +5,37 @@ output pulse, clk_1hz;
 
 reg [31:0] clk_var, hybrid;  
 reg p_out; 
-reg [7:0] r_reg;
-wire [7:0] r_nxt;
-reg clk_track;
+reg [7:0] r_reg, r_reg_1hz;
+wire [7:0] r_nxt, r_nxt_1hz;
+reg clk_track, clk_track_1hz;
+reg [31:0] hybrid_array [8:0]; 
+reg [31:0] hybrid_cnt; 
+reg [31:0] hybrid_loop; 
+reg [7:0] i; 
 
+initial 
+begin 
+hybrid_array[0] = 20; // 20 
+hybrid_array[1] = 32; 
+hybrid_array[2] = 66; 
+hybrid_array[3] = 26; 
+hybrid_array[4] = 70; 
+hybrid_array[5] = 30; 
+hybrid_array[6] = 20; 
+hybrid_array[7] = 30; 
+hybrid_array[8] = 32; 
+
+end 
 
 // choose output based on mode 
 
 always @(posedge clk)
 begin 
 case (mode)
-	2'b00: clk_var = 16;   
-	2'b01: clk_var = 32; 
-	2'b10: clk_var = 64;
-	2'b11: clk_var = hybrid;
+	2'b00: clk_var = 16;   //32hz 100e6hz / 32hz = 0x170784
+	2'b01: clk_var = 32;   //64hz 100e6hz/ 64hz = 
+	2'b10: clk_var = 64;   //128hz 100e6hz / 128hz = 
+	2'b11: begin clk_var = hybrid; end 
 endcase
 end 
 
@@ -44,12 +61,43 @@ begin
       r_reg <= r_nxt;
 end
 
+//hybrid generate 
+always @(posedge clk_1hz)
+begin 
+//TODO: intialize hybrid count somehow 
+if(hybrid_cnt <= 9) begin i = 0; hybrid = hybrid_loop; end
+else if(hybrid_cnt < 74) begin hybrid = 70; end 
+else if(hybrid_cnt < 80) begin hybrid = 34; end 
+else if(hybrid_cnt < 144) begin hybrid = 124; end 
+else hybrid = 0; 
 
+for(i = 0 ; i < 10; i = i + 1)
+begin 
+hybrid_loop = hybrid_array[i];
+end 
+
+end 
 
 //1Hz clk div 
+ assign r_nxt_1hz = r_reg_1hz+1;   	      
+ assign clk_1hz =  clk_track_1hz; 
+
 always @(posedge clk)
 begin 
-
+  if (rst)
+     begin
+        r_reg_1hz <= 0;
+	clk_track_1hz <= 1'b0;
+     end
+ 
+  else if (r_nxt_1hz == 64'd100000000) //1hz: 100000000/100000000 
+ 	   begin
+	     r_reg_1hz <= 0;
+	     clk_track_1hz <= ~clk_track_1hz;
+	   end
+ 
+  else 
+      r_reg_1hz <= r_nxt_1hz;
 end 
 
 
